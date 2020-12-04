bd05cf413adfb6447b367dae2e1d77472b7d5653
David Halliwell <david.halliwell@cloudreach.com>
2020-12-04 10:20:21 +0000

    AoC - Day 4

--- 4/4.py
+++ 4/4.py
@@ -1,16 +1,132 @@
 from aocd import data
+import re
+import numpy
+
+'''
+
+    byr (Birth Year) - four digits; at least 1920 and at most 2002.
+    iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+    eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+    hgt (Height) - a number followed by either cm or in:
+        If cm, the number must be at least 150 and at most 193.
+        If in, the number must be at least 59 and at most 76.
+    hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+    ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+    pid (Passport ID) - a nine-digit number, including leading zeroes.
+    cid (Country ID) - ignored, missing or not.
+'''
+re_xyr = re.compile('^\d{4}$')
+re_hgt = re.compile('^(\d+)(cm|in)$')
+re_hcl = re.compile('^#[\da-z]{6}$')
+re_ecl = re.compile('^(amb|blu|brn|gry|grn|hzl|oth)$')
+re_pid = re.compile('^\d{9}$')
+
+
+# I had to use the test data for debugging my code
+def get_test_input():
+    raw_data = open("4/test_data", "r")
+    rawdata = raw_data.read()
+    return [line for line in rawdata.read().split("\n\n")]
 
 
 def get_input():
-    return [line for line in data.splitlines()]
+    return [line for line in data.split("\n\n")]
 
 
-forest = get_input()
+def validate(fields):
+    tf = 0
+    val_data = {}
+    for field in fields:
+        if field == '':
+            continue
+        [header, data] = field.split(":")
+        if header == 'byr':
+            if re_xyr.match(data):
+                idata = int(data)
+                if 1920 <= idata <= 2002:
+                    val_data[header] = True
+                    tf += 1
+                else:
+                    val_data[header] = False
+        if header == 'iyr':
+            if re_xyr.match(data):
+                if 2010 <= int(data) <= 2020:
+                    val_data[header] = True
+                    tf += 1
+                else:
+                    val_data[header] = False
+        if header == 'eyr':
+            if re_xyr.match(data):
+                if 2020 <= int(data) <= 2030:
+                    val_data[header] = True
+                    tf += 1
+                else:
+                    val_data[header] = False
+        if header == 'hgt':
+            if re_hgt.match(data):
+                hgt_matches = re_hgt.match(data)
+                if hgt_matches[2] == "cm":
+                    if 150 <= int(hgt_matches[1]) <= 193:
+                        tf += 1
+                        val_data[header] = True
+                    else:
+                        val_data[header] = False
+                if hgt_matches[2] == "in":
+                    if 59 <= int(hgt_matches[1]) <= 76:
+                        tf += 1
+                        val_data[header] = True
+                    else:
+                        val_data[header] = False
+        if header == 'hcl':
+            if re_hcl.match(data):
+                tf += 1
+                val_data[header] = True
+            else:
+                val_data[header] = False
+        if header == 'ecl':
+            if re_ecl.match(data):
+                tf += 1
+                val_data[header] = True
+            else:
+                val_data[header] = False
+        if header == 'pid':
+            if re_pid.match(data):
+                tf += 1
+                val_data[header] = True
+            else:
+                val_data[header] = False
+    type(val_data)
+    return tf == 7
+
+
+def simple_fields(documents, validate_flag):
+    count = 0
+    for document in documents:
+        fields = re.split(' |\n', document)
+        if len(fields) == 8:
+            if validate_flag:
+                if validate(fields):
+                    count += 1
+            else:
+                count += 1
+        elif len(fields) == 7:
+            def func(x): return re.match('cid:', x)
+            testo = list(map(func, fields))
+            if not numpy.any(testo):
+                if validate_flag:
+                    if validate(fields):
+                        count += 1
+                else:
+                    count += 1
+    return count
+
+
+documents = get_input()
 
 # Part 1
-
-# print("Part 1: {}".format(answer1))
+valid1 = simple_fields(documents, False)
+print("Part 1: {}".format(valid1))
 
 # Part 2
-
-# print("Part 2: {}".format(answer2))
+valid2 = simple_fields(documents, True)
+print("Part 2: {}".format(valid2))
--- 4/README.md
+++ 4/README.md
@@ -2,6 +2,15 @@
 
 [https://adventofcode.com/2020/day/4](https://adventofcode.com/2020/day/4)
 
+I was woken by terrifying loud #thundersnow at 4:55am, so I figured it was a sign that I should start coding ay 5am (when the challenge was released!)  I found implementing and debugging the second part pretty tedious, and I had to debug it using the test data in the challenge description.  Luckily I managed to get some sleep afterwards.
+
+I'm not that happy with my scrappy code (so many conditionals!) so I may re-work this.
+
 TIL:
 
-- Nothing new to speak of today
+- I'm sure it's not the most effective approach, but this was the first time I have defined a function and applied to an iterable using map, then collected the results together using numpy:
+```python
+def func(x): return re.match('cid:', x)
+cid_map = list(map(func, fields))
+if not numpy.any(cid_map):
+```
